1dd9eb817069667d06cb49ec8e5ab9447f2c16d3
Kodai <0711dr5@gmail.com>
2024-10-19 23:09:11 +0900

    fix:composables

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,21 @@
+<script setup>
+import { useCounter } from "~/composables/useCounter";
+
+const { count, inc, dec } = useCounter(0);
+</script>
+
 <template>
   <div>
+    <div>
+      <img src="/icon.png" alt="Nuxt3 Icon" />
+    </div>
     <h1>Main Page</h1>
     <div>
-      <img src="/assets/icon.png" alt="Nuxt3 Icon" />
+      <p>Count: {{ count }}</p>
+    </div>
+    <div>
+      <button @click="() => inc()">increase</button>
+      <button @click="() => dec()">decrease</button>
     </div>
   </div>
 </template>
