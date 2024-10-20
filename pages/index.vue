6e9ce118004587059d006355571799f2b4080a30
Kodai <0711dr5@gmail.com>
2024-10-20 19:46:46 +0900

    fix:prisma

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,30 @@
 <script setup>
-const { data } = useFetch("/api/hello");
+const task = ref("");
+const { data: tasks, refresh } = useFetch("/api/task"); // refreshを取得
+
+const addTask = async () => {
+  await $fetch("/api/task", {
+    method: "post",
+    body: { task: task.value },
+  });
+  await refresh(); // データを再取得してリフレッシュ
+  task.value = "";
+};
 </script>
+
 <template>
   <div>
     <h1>Main Page</h1>
-    <h2>{{ data }}</h2>
+    <ul>
+      <li v-for="task in tasks" :key="task.id">{{ task.task }}</li>
+    </ul>
+    <form @submit.prevent="addTask">
+      <div>
+        <input v-model="task" />
+      </div>
+      <div>
+        <button type="submit">タスクを登録</button>
+      </div>
+    </form>
   </div>
 </template>
